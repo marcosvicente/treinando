f17568264ed6c515117b0f76c3c8062dce4ed91b
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2017-02-07 14:31:38 -0200

    View index criada

---
+++ cake/app/View/Compras/index.php
@@ -0,0 +1,10 @@
+<h2>Compras</h2>
+    <table>
+    <tr>
+      <th>nome</th>
+<th>test</th>
+<th>a</th>
+</tr><?php foreach ($Compras as $lista_Compras): ?><tr><td><?php echo $lista_nome[Compras][nome]; ?></td><td><?php echo $lista_test[Compras][test]; ?></td><td><?php echo $lista_a[Compras][a]; ?></td>
+    </tr>
+    </table>
+    
--- scripts.py
+++ scripts.py
@@ -9,22 +9,57 @@ def __init__():
 # Define a main() function that prints a little greeting.
 
 
-def criarModel():
+def criarView():
+    # criar Pasta
     if os.path.exists('cake/app/View/' + nome):
         print("Ja exites")
     else:
-        os.mkdir('cake/app/View/' + nome)
+        os.mkdir('cake/app/View/' + nome )
+def criarViewIndex():
+    f = open('cake/app/View/' +nome +'/index.php', "w+")
+    f.write("<h2>"+ nome + "</h2>")
+    f.write("""
+    <table>
+    <tr>
+      """)
+    numero_tabelas = len(sys.argv)
+    for numero_tabelas in range(2, numero_tabelas):
+        tabelas_list = todas_tabelas[numero_tabelas]
+        tabelas_dados = re.search('(?<!:)\w+', tabelas_list)
+        tabelas = tabelas_dados.group()
+        f.write("<th>"+ tabelas + "</th>\n")
+
+    f.write("</tr>")
+    f.write("<?php foreach ($"+nome+" as $lista_"+nome+"): ?>")
+    f.write("<tr>")
+    numero_tabelas = len(sys.argv)
+    for numero_tabelas in range(2, numero_tabelas):
+        tabelas_list = todas_tabelas[numero_tabelas]
+        tabelas_dados = re.search('(?<!:)\w+', tabelas_list)
+        tabelas = tabelas_dados.group()
+        f.write("<td><?php echo $lista_"+tabelas+"["+nome+"]["+tabelas+"]; ?></td>")
+    f.write("""
+    </tr>
+    </table>
+    """)
+
+    f.close()
 
 def comandos():
   # Get the name from the command line, using 'World' as a fallback.
-    numero_tabelas =len(sys.argv)
+    global numero_tabelas
+    numero_tabelas = len(sys.argv)
 
     global nome
     nome = sys.argv[1]
+
+    global todas_tabelas
     todas_tabelas = sys.argv
     #tabelas = sys.argv
 
     print('create table ' + nome +'(')
+    #id
+    print('id int primary key not null')
     for numero_tabelas in range(2, numero_tabelas):
         tabelas_list = todas_tabelas[numero_tabelas]
 
@@ -32,6 +67,8 @@ def comandos():
         tipo = re.search('(?<=:)\w+', tabelas_list)
 
         tipo_de_dados_contruindo = tipo.group(0)
+
+        global tipo_de_dados
 
         if tipo_de_dados_contruindo == 'string':
             tipo_de_dados = ' varchar '
@@ -42,12 +79,14 @@ def comandos():
         else:
             print('deu ruim')
         #Nome tabela
+
         tabelas_dados = re.search('(?<!:)\w+', tabelas_list)
+        global tabelas
+
         tabelas = tabelas_dados.group()
 
 
         # saida de dados
-        #print(tabelas[numero_tabelas] + tipo_de_dados + " not null,")
         print(tabelas + tipo_de_dados + " not null,")
 
         numero_tabelas -= numero_tabelas
@@ -57,8 +96,8 @@ def comandos():
 
 def main():
     comandos()
-    criarModel()
-
+    criarView()
+    criarViewIndex()
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
     main()
