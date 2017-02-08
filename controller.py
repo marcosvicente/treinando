ab650c84a29bb3d0dd12c5bd02d234eb578639a6
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2017-02-07 22:21:08 -0200

    :Terminado controller

--- controller.py
+++ controller.py
@@ -24,11 +24,26 @@ def criarController():
     f.write("class "+nome +"sController extends AppController{ \n")
 
     f.write("public $helpers = array('Html', 'Form', 'Flash');\n")
+
     f.write("public $components = array('Flash');\n")
+    print("Controller "+nome+" --------- Criado")
+    numero_tabelas = len(sys.argv)
     for numero_tabelas in range(2, numero_tabelas):
-        f.write("public function "+todas_tabelas[numero_tabelas]+" (){} ;\n")
+        f.write("public function "+todas_tabelas[numero_tabelas]+"(){} ;\n")
+        print(todas_tabelas[numero_tabelas] + " --------------------- Ok")
+
+    f.write("} \n")
+    f.write("?> \n")
 
     f.close()
+
+def criarTodasView():
+    numero_tabelas = len(sys.argv)
+    for numero_tabelas in range(2, numero_tabelas):
+        f = open('cake/app/View/'+ nome +'s/'+todas_tabelas[numero_tabelas] +'.ctp', "w+")
+        f.write(todas_tabelas[numero_tabelas]+" View")
+        f.write("\n")
+        f.close()
 
 
 def comandos():
@@ -43,31 +58,10 @@ def comandos():
     todas_tabelas = sys.argv
     #tabelas = sys.argv
 
-    print("Criar table:\n")
 
-    print('create table ' + nome +'(')
     for numero_tabelas in range(2, numero_tabelas):
         tabelas_list = todas_tabelas[numero_tabelas]
 
-        # Tipo de dados
-        tipo = re.search('(?<=:)\w+', tabelas_list)
-
-        tipo_de_dados_contruindo = tipo.group(0)
-
-        global tipo_de_dados
-
-        if tipo_de_dados_contruindo == 'string':
-            tipo_de_dados = ' varchar(255) '
-        elif tipo_de_dados_contruindo == 'integer':
-            tipo_de_dados = ' int '
-        elif tipo_de_dados_contruindo == 'data':
-            tipo_de_dados = ' data '
-        elif tipo_de_dados_contruindo == 'float':
-            tipo_de_dados = ' float '
-        elif tipo_de_dados_contruindo == 'text':
-            tipo_de_dados = ' text '
-        else:
-            print('deu ruim')
         #Nome tabela
 
         tabelas_dados = re.search('(?<!:)\w+', tabelas_list)
@@ -76,21 +70,16 @@ def comandos():
         tabelas = tabelas_dados.group()
 
 
-        # saida de dados
-        print(tabelas + tipo_de_dados + " not null,")
-
         numero_tabelas -= numero_tabelas
-    print('id int primary key not null auto_increment')
 
-    print(')')
-    print("\n")
+
 
 
 
 def main():
     comandos()
     criarView()
-
+    criarTodasView()
     criarController()
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
